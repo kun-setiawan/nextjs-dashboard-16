008cab28e55dcf2cfa477b157c1c8d92edc147db
kun-setiawan <37320639+kun-setiawan@users.noreply.github.com>
2026-06-17 15:12:07 +0700

    upload profile picture

--- create-bucket.mjs
+++ create-bucket.mjs
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import * as dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
-dotenv.config({ path: '.env' });
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
